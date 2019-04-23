b4e741912e979df9693faefd9f12d35e0ebb5792
Abdulkhamid Muminov <a.muminov@innopolis.ru>
2019-04-23 20:45:09 +0300

    minor changes

--- routes/visitors.js
+++ routes/visitors.js
@@ -8,8 +8,10 @@ const createRouter = require('@arangodb/foxx/router');
 const Visitor = require('../models/visitor');
 
 const Visitors = module.context.collection('Visitors');
+const Doctors = module.context.collection('Staff');
+const Facilities = module.context.collection('Facilities')
 const keySchema = joi.string().required()
-.description('The key of the visitor');
+  .description('The key of the visitor');
 
 const ARANGO_NOT_FOUND = errors.ERROR_ARANGO_DOCUMENT_NOT_FOUND.code;
 const ARANGO_DUPLICATE = errors.ERROR_ARANGO_UNIQUE_CONSTRAINT_VIOLATED.code;
@@ -25,30 +27,59 @@ router.tag('visitor');
 
 
 router.get(function (req, res) {
-  res.send(Visitors.all());
-}, 'list')
-.response([Visitor], 'A list of Visitors.')
-.summary('List all Visitors')
-.description(dd`
+    res.send(Visitors.all());
+  }, 'list')
+  .response([Visitor], 'A list of Visitors.')
+  .summary('List all Visitors')
+  .description(dd `
   Retrieves a list of all Visitors.
 `);
 
 
+router.post(function (req, res) {
+    const visitor = req.body;
+    let meta;
+    try {
+      meta = Visitors.save(visitor);
+    } catch (e) {
+      if (e.isArangoError && e.errorNum === ARANGO_DUPLICATE) {
+        throw httpError(HTTP_CONFLICT, e.message);
+      }
+      throw e;
+    }
+    Object.assign(visitor, meta);
+    res.status(201);
+    res.set('location', req.makeAbsolute(
+      req.reverse('detail', {
+        key: visitor._key
+      })
+    ));
+    res.send(visitor);
+  }, 'create')
+  .body(Visitor, 'The visitor to create.')
+  .response(201, Visitor, 'The created visitor.')
+  .error(HTTP_CONFLICT, 'The visitor already exists.')
+  .summary('Create a new visitor')
+  .description(dd `
+  Creates a new visitor from the request body and
+  returns the saved document.
+`)
+
 router.get(':key', function (req, res) {
   const key = req.pathParams.key;
-  let fascilities
+  let facilities
   try {
-    fascilities = Fascilities.document(key);
+    facilities = Facilities.document(key);
   } catch (e) {
     if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
       throw httpError(HTTP_NOT_FOUND, e.message);
     }
     throw e;
   }
-  res.send(fascilities);
+  res.send(facilities);
 }, 'detail')
 .pathParam('key', keySchema)
-.response(Fascilities, 'The fascility.')
+.response(Facilities, 'The fascility.')
 .summary('Fetch a fascility')
 .description(dd `
 Retrieves a fascility by its key.
@@ -76,125 +107,97 @@ Retrieves a doctor by its key.
 `);
 
 
-router.post(function (req, res) {
-  const visitor = req.body;
-  let meta;
-  try {
-    meta = Visitors.save(visitor);
-  } catch (e) {
-    if (e.isArangoError && e.errorNum === ARANGO_DUPLICATE) {
-      throw httpError(HTTP_CONFLICT, e.message);
-    }
-    throw e;
-  }
-  Object.assign(visitor, meta);
-  res.status(201);
-  res.set('location', req.makeAbsolute(
-    req.reverse('detail', {key: visitor._key})
-  ));
-  res.send(visitor);
-}, 'create')
-.body(Visitor, 'The visitor to create.')
-.response(201, Visitor, 'The created visitor.')
-.error(HTTP_CONFLICT, 'The visitor already exists.')
-.summary('Create a new visitor')
-.description(dd`
-  Creates a new visitor from the request body and
-  returns the saved document.
-`);
-
-
 router.get(':key', function (req, res) {
-  const key = req.pathParams.key;
-  let visitor
-  try {
-    visitor = Visitors.document(key);
-  } catch (e) {
-    if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
-      throw httpError(HTTP_NOT_FOUND, e.message);
+    const key = req.pathParams.key;
+    let visitor
+    try {
+      visitor = Visitors.document(key);
+    } catch (e) {
+      if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
+        throw httpError(HTTP_NOT_FOUND, e.message);
+      }
+      throw e;
     }
-    throw e;
-  }
-  res.send(visitor);
-}, 'detail')
-.pathParam('key', keySchema)
-.response(Visitor, 'The visitor.')
-.summary('Fetch a visitor')
-.description(dd`
+    res.send(visitor);
+  }, 'detail')
+  .pathParam('key', keySchema)
+  .response(Visitor, 'The visitor.')
+  .summary('Fetch a visitor')
+  .description(dd `
   Retrieves a visitor by its key.
 `);
 
 
 router.put(':key', function (req, res) {
-  const key = req.pathParams.key;
-  const visitor = req.body;
-  let meta;
-  try {
-    meta = Visitors.replace(key, visitor);
-  } catch (e) {
-    if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
-      throw httpError(HTTP_NOT_FOUND, e.message);
+    const key = req.pathParams.key;
+    const visitor = req.body;
+    let meta;
+    try {
+      meta = Visitors.replace(key, visitor);
+    } catch (e) {
+      if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
+        throw httpError(HTTP_NOT_FOUND, e.message);
+      }
+      if (e.isArangoError && e.errorNum === ARANGO_CONFLICT) {
+        throw httpError(HTTP_CONFLICT, e.message);
+      }
+      throw e;
     }
-    if (e.isArangoError && e.errorNum === ARANGO_CONFLICT) {
-      throw httpError(HTTP_CONFLICT, e.message);
-    }
-    throw e;
-  }
-  Object.assign(visitor, meta);
-  res.send(visitor);
-}, 'replace')
-.pathParam('key', keySchema)
-.body(Visitor, 'The data to replace the visitor with.')
-.response(Visitor, 'The new visitor.')
-.summary('Replace a visitor')
-.description(dd`
+    Object.assign(visitor, meta);
+    res.send(visitor);
+  }, 'replace')
+  .pathParam('key', keySchema)
+  .body(Visitor, 'The data to replace the visitor with.')
+  .response(Visitor, 'The new visitor.')
+  .summary('Replace a visitor')
+  .description(dd `
   Replaces an existing visitor with the request body and
   returns the new document.
 `);
 
 
 router.patch(':key', function (req, res) {
-  const key = req.pathParams.key;
-  const patchData = req.body;
-  let visitor;
-  try {
-    Visitors.update(key, patchData);
-    visitor = Visitors.document(key);
-  } catch (e) {
-    if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
-      throw httpError(HTTP_NOT_FOUND, e.message);
+    const key = req.pathParams.key;
+    const patchData = req.body;
+    let visitor;
+    try {
+      Visitors.update(key, patchData);
+      visitor = Visitors.document(key);
+    } catch (e) {
+      if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
+        throw httpError(HTTP_NOT_FOUND, e.message);
+      }
+      if (e.isArangoError && e.errorNum === ARANGO_CONFLICT) {
+        throw httpError(HTTP_CONFLICT, e.message);
+      }
+      throw e;
     }
-    if (e.isArangoError && e.errorNum === ARANGO_CONFLICT) {
-      throw httpError(HTTP_CONFLICT, e.message);
-    }
-    throw e;
-  }
-  res.send(visitor);
-}, 'update')
-.pathParam('key', keySchema)
-.body(joi.object().description('The data to update the visitor with.'))
-.response(Visitor, 'The updated visitor.')
-.summary('Update a visitor')
-.description(dd`
+    res.send(visitor);
+  }, 'update')
+  .pathParam('key', keySchema)
+  .body(joi.object().description('The data to update the visitor with.'))
+  .response(Visitor, 'The updated visitor.')
+  .summary('Update a visitor')
+  .description(dd `
   Patches a visitor with the request body and
   returns the updated document.
 `);
 
 
 router.delete(':key', function (req, res) {
-  const key = req.pathParams.key;
-  try {
-    Visitors.remove(key);
-  } catch (e) {
-    if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
-      throw httpError(HTTP_NOT_FOUND, e.message);
+    const key = req.pathParams.key;
+    try {
+      Visitors.remove(key);
+    } catch (e) {
+      if (e.isArangoError && e.errorNum === ARANGO_NOT_FOUND) {
+        throw httpError(HTTP_NOT_FOUND, e.message);
+      }
+      throw e;
     }
-    throw e;
-  }
-}, 'delete')
-.pathParam('key', keySchema)
-.response(null)
-.summary('Remove a visitor')
-.description(dd`
+  }, 'delete')
+  .pathParam('key', keySchema)
+  .response(null)
+  .summary('Remove a visitor')
+  .description(dd `
   Deletes a visitor from the database.
 `);
