1a0281c6748eba4761959c2e7dae2a66e57ae483
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-04-23 18:35:08 +0300

    Fixes.

--- routes/patients.js
+++ routes/patients.js
@@ -80,8 +80,7 @@ router.post('/signup', function (req, res) {
     patient.authData = auth.create(patient.password);
     delete patient.password;
     delete patient.perms;
-    const coordinates = db._query(aql`FOR s IN Addresses FILTER s.address == ${patient.address.street + ', ' + patient.address.building} RETURN s.coordinate`).next() || [0.0, 0.0];
-    console.log(coordinates);
+    const coordinates = db._query(aql`FOR s IN Addresses FILTER s.address == ${patient.address.street + ', ' + patient.address.building} RETURN s.coordinate`).next() || [[0.0, 0.0]];
     patient.residential_area = coordinates[0]; // TODO: Получить координаты из patient.address
     const meta = patients.save(patient);
     Object.assign(patient, meta);
